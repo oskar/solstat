e9e82cd5878b2bf9d6e9f13038fa651cf4c41749
Oskar Hermansson <oskar.hermansson@gmail.com>
2013-02-09 00:01:50 +0100

    Added FileHelper for finding solution file

---
+++ Source/Solstat/Solstat.Tests/FileHelperTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solstat.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solstat.Tests
+{
+    [TestClass]
+    public class FileHelperTests
+    {
+        [TestMethod]
+        public void ShouldReturnNothingForInvalidDirectory()
+        {
+            Assert.IsTrue(string.IsNullOrEmpty(FileHelper.FindSolutionFile("this-directory-does-not-exist")));
+        }
+
+        [TestMethod]
+        public void ShouldFindSolutionFileStartingInCurrentDirectory()
+        {
+            Assert.IsFalse(string.IsNullOrEmpty(FileHelper.FindSolutionFile(Environment.CurrentDirectory)));
+        }
+    }
+}
--- Source/Solstat/Solstat.Tests/SolutionParserTests.cs
+++ Source/Solstat/Solstat.Tests/SolutionParserTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Solstat.Library;
+using System.Diagnostics;
+using System.IO;
 
 namespace Solstat.Tests
 {
@@ -19,6 +21,17 @@ namespace Solstat.Tests
         public void ShouldThrowExceptionWhenSolutionFileArgumentIsEmpty()
         {
             var parser = new SolutionParser("");
+        }
+
+        [TestMethod]
+        public void ShouldReturnListOfProjects()
+        {
+            var solutionFile = FileHelper.FindSolutionFile(Environment.CurrentDirectory);
+
+            var parser = new SolutionParser(solutionFile);
+            var projects = parser.Parse();
+
+            Assert.IsNotNull(projects);
         }
     }
 }
