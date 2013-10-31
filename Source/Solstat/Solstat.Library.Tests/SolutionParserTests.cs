edc620a496089dc8336b73b7ade30f3bdabbab87
Oskar Hermansson <oskar.hermansson@gmail.com>
2013-10-31 16:47:16 +0100

    Refactored tests using NUnit

--- Source/Solstat/Solstat.Library.Tests/SolutionParserTests.cs
+++ Source/Solstat/Solstat.Library.Tests/SolutionParserTests.cs
@@ -1,34 +1,41 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
 
 namespace Solstat.Library.Tests
 {
-  [TestClass]
+  [TestFixture]
   public class SolutionParserTests
   {
-    [TestMethod]
-    [ExpectedException(typeof(ArgumentNullException))]
-    public void ShouldThrowExceptionWhenSolutionFileArgumentIsNull()
+    private readonly string solutionFile_ = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data",
+                                                         "SolstatTestData.sln");
+
+    [Test]
+    public void Should_throw_exception_when_solution_file_argument_is_null_or_empty()
     {
-      var parser = new SolutionParser(null);
+      Assert.Throws<ArgumentNullException>(() => new SolutionParser(null));
+      Assert.Throws<ArgumentNullException>(() => new SolutionParser(""));
     }
 
-    [TestMethod]
-    [ExpectedException(typeof(ArgumentNullException))]
-    public void ShouldThrowExceptionWhenSolutionFileArgumentIsEmpty()
+    [Test]
+    public void Should_contain_three_projects()
     {
-      var parser = new SolutionParser("");
-    }
-
-    [TestMethod]
-    public void ShouldReturnListOfProjects()
-    {
-      var solutionFile = FileHelper.FindSolutionFile(Environment.CurrentDirectory);
-
-      var parser = new SolutionParser(solutionFile);
+      var parser = new SolutionParser(solutionFile_);
       var projects = parser.Parse();
-
       Assert.IsNotNull(projects);
+      Assert.That(projects.Count, Is.EqualTo(3));
+    }
+
+    [Test]
+    public void Should_contain_one_wpf_project()
+    {
+      var parser = new SolutionParser(solutionFile_);
+      var projects = parser.Parse();
+      Assert.IsNotNull(projects);
+
+      var wpfProject = projects.Single(p => p.ProjectName == "WpfApplication1");
+      Assert.That(wpfProject.TargetFrameworkVersion, Is.EqualTo(".NET 4.5"));
     }
   }
 }
